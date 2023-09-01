fd31ff1912c00c0a0e319edbfd9c586c3cb7b771
jharn <jharn@DESKTOP-3PO14J4>
2023-09-01 22:43:00 +1000

    add some changes in files like login controller and change css.

---
+++ src/main/java/Controller/LoginController.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import Entity.User;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author jharn
+ */
+public class LoginController {
+
+    public String authenticateUser(User loginBean) {
+
+        try {
+
+            String userName = loginBean.getEmail(); //Assign user entered values to temporary variables.
+            String password = loginBean.getPassword();
+            DatabaseConnection dbConnection = new DatabaseConnection();
+            Connection connection = (Connection) dbConnection;
+            Statement statement = null;
+            ResultSet resultSet = null;
+            String userNameDB = "";
+            String passwordDB = "";
+            try {
+                statement = connection.createStatement(); //Statement is used to write queries. Read more about it.
+                resultSet = statement.executeQuery("select email,password from users"); //the table name is users and userName,password are columns. Fetching all the records and storing in a resultSet.
+                while (resultSet.next()) // Until next row is present otherwise it return false
+                {
+                    userNameDB = resultSet.getString("email"); //fetch the values present in database
+                    passwordDB = resultSet.getString("password");
+                    if (userName.equals(userNameDB) && password.equals(passwordDB)) {
+                        return "SUCCESS"; ////If the user entered values are already present in the database, which means user has already registered so return a SUCCESS message.
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "Invalid user credentials"; // Return appropriate message in case of failure
+    }
+}
